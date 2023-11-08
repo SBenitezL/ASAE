1580b085d14d1ea25ba9f4813eb4836b6488fda4
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-11-08 14:41:33 -0500

    Libreria y model personas

--- src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Personas.java
+++ src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Personas.java
@@ -1,5 +1,39 @@
 package co.edu.unicauca.validacion_errores_back.core.models;
 
-public class Personas {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Personas{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
     
+    private String tipoIdentificacion;
+    private String numeroIdentificacion;
+    private String nombres;
+    private String apellidos;
+
+
+    public Personas(String tipoIdentificacion,String  numeroIdentificacion, String nombres, String apellidos)
+    {
+        this.tipoIdentificacion = tipoIdentificacion;
+        this.numeroIdentificacion = numeroIdentificacion;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
 }
