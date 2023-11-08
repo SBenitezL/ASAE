a28f4e579ebe7f7a3df4a7d475a6bd43daf2090d
Santiago Benitez Lopez <sbenitez1607@gmail.com>
2023-11-08 15:39:48 -0500

    Fix: Docente Entity

--- src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Docente.java
+++ src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Docente.java
@@ -1,7 +1,18 @@
 package co.edu.unicauca.validacion_errores_back.core.models;
 
-@Entity
-@Entity
+import java.util.ArrayList;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.OneToOne;
+@Entity(name = "docentes")
 @PrimaryKeyJoinColumn(name = "idPersona")
 @Getter
 @Setter
@@ -19,8 +30,8 @@ public class Docente extends Personas{
     @ManyToMany(fetch=FetchType.LAZY)
     @JoinTable(name="Docentes_Publicaciones",
         joinColumns = @JoinColumn(name = "idPersona"),
-        InverseJoinColumns = @JoinColumn(name = "idPublicacion"))
-    private List<Publicacion> publicaciones;
+        inverseJoinColumns = @JoinColumn(name = "idPublicacion"))
+    private ArrayList<Publicacion> publicaciones;
 
     @OneToOne(mappedBy = "objDocente",fetch=FetchType.EAGER)
     private Direccion objDireccion;
@@ -35,11 +46,6 @@ public class Docente extends Personas{
         this.correo = correo;
         this.vinculacion = vinculacion;
         this.departamento = departamento;
+        this.publicaciones = new ArrayList<Publicacion>();
     }
-
-    @ManyToMany(fetch=FetchType.LAZY)
-    @JoinTable(name="Docentes_Publicaciones",
-        joinColumns = @JoinColumn(name = "idPersona"),
-        InverseJoinColumns = @JoinColumn(name = "idPublicacion"))
-    private List<Publicacion> publicaciones;
 }
