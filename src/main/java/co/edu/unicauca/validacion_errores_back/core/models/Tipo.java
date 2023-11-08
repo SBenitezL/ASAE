2f5c0dbbca7e94317dfc834e5945c92ed94bca78
Simon <siguzman@unicauca.edu.co>
2023-11-08 15:22:07 -0500

    asdasd

--- src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Tipo.java
+++ src/main/java/co/edu/unicauca/validacion_errores_back/core/models/Tipo.java
@@ -1,5 +1,39 @@
 package co.edu.unicauca.validacion_errores_back.core.models;
 
+|import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="Tipos")
 public class Tipo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idTipo;
+    @Column(nullable = false, length = 45)
+    private String nombre;
+    @Column(unique = true, nullable = false, length = 45)
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "objTipo")
+
+    public Integer getIdTipo() {
+        return idTipo;
+    }
+    public void setIdTipo(Integer idTipo) {
+        this.idTipo = idTipo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
